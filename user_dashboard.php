85e20f5d7c0d48098a9b923af99392eb7176da8d
MiaS-Git <55926870+MiaS-Git@users.noreply.github.com>
2021-05-01 15:18:45 -0400

    Add files via upload

---
+++ user_dashboard.php
@@ -0,0 +1,58 @@
+<?php
+
+include('include/connect.php');
+$user_id = 1;
+$sql = "SELECT * FROM `users` WHERE uid=$uid";
+//echo $sql;exit;
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_array($result);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Dashboard</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+
+    <style>
+        body {
+            position: auto;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+
+        .container {
+            padding-top: 20px;
+        }
+
+        .btn {
+            border-radius: 0px;
+        }
+
+        .jumbotron {
+            border-radius: 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-5">
+                <?php echo "WELCOME "; ?> <b> <?php echo strtoupper($row['uname']); ?>! </b>
+            </h1>
+            <p class="lead">List & Rent your Space for Parking.</p>
+            <center>
+                <hr class="my-4">
+                <a class="btn btn-primary btn-lg" href="check_booking.php?uid=<?php echo $row['uid'] ?>" role="button">Book</a>
+            </center>
+        </div>
+    </div>
+
+</body>
+
+</html>
