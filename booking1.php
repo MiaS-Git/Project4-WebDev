bc0387f76b6f40517e0637ad69e1dd9ac924343e
MiaS-Git <55926870+MiaS-Git@users.noreply.github.com>
2021-05-03 04:01:42 -0400

    Update booking1.php

--- booking1.php
+++ booking1.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
-    header("location:user_login.php");
+    header("location:login.php");
 }
 include('include/connect.php');
 
 $id = $_GET['id'];
-//POST DATA from select_datetime.php
+//POST from select_datetime.php
 $slot_date = $_POST['slot_date'];
 $start_time = $_POST['start_time'];
 $no_of_hr = $_POST['no_of_hr'];
@@ -22,9 +22,9 @@ $exit_time = date('H:i', strtotime($start_time . '+ ' . $no_of_hr . ' hour'));
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Dashboard</title>
-    <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <link href="assets/css/style.css" rel="stylesheet" />
+    <title>Confirm a spot</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
     <style>
         .btn {
             border-radius: 0;
@@ -54,18 +54,18 @@ $exit_time = date('H:i', strtotime($start_time . '+ ' . $no_of_hr . ' hour'));
         <h3>Select a spot</h3>
         <div class="row">
             <?php
-            $sql_slot = "SELECT * FROM slot_master";
+            $sql_slot = "SELECT * FROM spots";
             $result_slot = mysqli_query($conn, $sql_slot);
-            while ($data = mysqli_fetch_array($result_slot)) {
-                if ($data['slot_status'] == 0) {
+            while ($data = mysqli_fetch_array($result_spot)) {
+                if ($data['spot_status'] == 0) {
             ?>
                     <div class="col-lg-2">
                         <hr>
-                        <a type=" button" class="btn btn-outline-primary btn-lg btn-block" href="source/confirm_slot.php?slot_id=<?php echo $data['slot_id']; ?>&&id=<?php echo $id; ?>">Spot
-                            <?php echo $data['slot_id']; ?>
+                        <a type=" button" class="btn btn-outline-primary btn-lg btn-block" href="include/confirm_spot.php?spot_id=<?php echo $data['spot_id']; ?>&&id=<?php echo $id; ?>">Spot
+                            <?php echo $data['spot_id']; ?>
                         </a>
                     </div>
-                    <?php } else if ($data['slot_status'] == 1) {
+                    <?php } else if ($data['spot_status'] == 1) {
                     $sql_time = "SELECT * FROM parking_details";
                     $result_time = mysqli_query($conn, $sql_time);
                     $row_time = mysqli_fetch_assoc($result_time);
@@ -76,15 +76,15 @@ $exit_time = date('H:i', strtotime($start_time . '+ ' . $no_of_hr . ' hour'));
                     ?>
                         <div class="col-lg-2">
                             <hr>
-                            <a type=" button" class="btn btn-outline-primary btn-lg btn-block" href="source/confirm_slot.php?slot_id=<?php echo $data['slot_id']; ?>&&id=<?php echo $id; ?>">Spot
-                                <?php echo $data['slot_id']; ?>
+                            <a type=" button" class="btn btn-outline-primary btn-lg btn-block" href="include/confirm_spot.php?spot_id=<?php echo $data['spot_id']; ?>&&id=<?php echo $id; ?>">Spot
+                                <?php echo $data['spot_id']; ?>
                             </a>
                         </div>
                     <?php } else { ?>
                         <div class="col-lg-2">
                             <hr>
                             <button type="button" class="btn btn-danger btn-lg btn-block" disabled>Spot
-                                <?php echo $data['slot_id']; ?>
+                                <?php echo $data['spot_id']; ?>
                             </button>
                         </div>
             <?php  }
@@ -94,9 +94,9 @@ $exit_time = date('H:i', strtotime($start_time . '+ ' . $no_of_hr . ' hour'));
             <p>
                 <small>
                     <img src="images/icon/red_block.png" height="20px" width="20px" style="border:1px solid;">
-                    slot already booked<br>
+                    spot already booked<br>
                     <img src=" images/icon/white_block.png" height="20px" width="20px" style="border:1px solid;">
-                    slot available
+                    spot available
                 </small>
             </p>
         </div>
@@ -114,11 +114,11 @@ if (isset($_POST['submit'])) {
     $sql_check = "SELECT * FROM parking_details WHERE id='$id'";
     $result_check = mysqli_query($conn, $sql_check);
     if (mysqli_fetch_assoc($result_check) == 0) {
-        $sql = "INSERT INTO `parking_details`(`uname`,`id`, `slot_date`, `start_time`,`no_of_hr`,`exit_time`,`parking_type`) VALUES ('$uname','$id','$slot_date','$start_time','$no_of_hr','$exit_time','$parking_type' )";
+        $sql = "INSERT INTO `parking_details`(`uname`,`id`, `spot_date`, `start_time`,`no_of_hr`,`exit_time`,`parking_type`) VALUES ('$uname','$id','$spot_date','$start_time','$no_of_hr','$exit_time','$parking_type' )";
 
         $result = mysqli_query($conn, $sql);
     } else if (mysqli_fetch_assoc($result_check) == 1) {
-        $sql = "UPDATE `parking_details` SET `slot_date`='$slot_date',`start_time`='$start_time',`no_of_hr`='$no_of_hr',`exit_time`='$exit_time', `parking_type`='$parking_type' WHERE $id='$id'";
+        $sql = "UPDATE `parking_details` SET `spot_date`='$spot_date',`start_time`='$start_time',`no_of_hr`='$no_of_hr',`exit_time`='$exit_time', `parking_type`='$parking_type' WHERE $id='$id'";
         $result = mysqli_query($conn, $sql);
     }
 }
