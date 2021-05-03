bf5419ee0a4caaed017569b1605f19b59693dc2a
MiaS-Git <55926870+MiaS-Git@users.noreply.github.com>
2021-05-03 04:10:18 -0400

    Update cancel_slot.php

--- include/cancel_slot.php
+++ include/cancel_slot.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 if(!isset($_SESSION['id']) || empty($_SESSION['id'])){
-	header("location:../user_login.php");
+	header("location:../login.php");
 }
 
 include('../include/connect.php');
 $id=$_GET['id'];
-$slot_id=$_GET['slot_id'];
+$slot_id=$_GET['spot_id'];
 //echo $slot_id;
 $sql="DELETE FROM `parking_details` WHERE id='$id'";
 $result=mysqli_query($conn,$sql);
 
-$sql2="UPDATE `slot_master` SET `slot_status`= 0 WHERE slot_id=$slot_id";
+$sql2="UPDATE `spots` SET `spot_status`= 0 WHERE spot_id=$spot_id";
 //echo $sql2;exit;
 header('location:../user_dashboard.php');
 ?>
