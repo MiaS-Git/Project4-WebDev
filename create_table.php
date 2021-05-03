64ba2547c313e04b7e435830c24044667cdae797
MiaS-Git <55926870+MiaS-Git@users.noreply.github.com>
2021-05-03 04:38:28 -0400

    Update create_table.php
    Added sql for parking details

--- create_table.php
+++ create_table.php
@@ -49,7 +49,31 @@
         .'`price` DECIMAL(11,2) NOT NULL,'
         .'PRIMARY KEY (`car_type`));';
 
-    mysqli_query($conn, $sql);          
+    mysqli_query($conn, $sql); 
+        
+$sql = 'CREATE TABLE `parking_details` ('
+          '`pid` int(2) NOT NULL,'
+        '  `spot_id` int(2) NOT NULL,'
+        '  `uname` varchar(50) NOT NULL,'
+          '`id` int(2) NOT NULL,'
+         ' `spot_date` varchar(10) NOT NULL,'
+         ' `start_time` varchar(6) NOT NULL,'
+         ' `no_of_hr` int(2) NOT NULL,'
+         ' `parking_type` int(3) NOT NULL,'
+         ' `exit_time` varchar(6) NOT NULL,'
+         ' `booking_code` varchar(100) NOT NULL'
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;'
+    
+    mysqli_query($conn, $sql); 
+
+  $sql =  'CREATE TABLE `spots` ('
+          '`spot_id` int(2) NOT NULL,'
+          '`spot_no` varchar(3) NOT NULL,'
+          '`spot_status` int(2) NOT NULL'
+        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;'
+      
+        mysqli_query($conn, $sql); 
+    $sql = "INSERT INTO `slot_master` (`slot_id`, `slot_no`, `slot_status`) VALUES (1, '1', 0),(2, '2', 0),(3, '3', 0),(4, '4', 0),(5, '5', 0),(6, '6', 0),(7, '7', 0),(8, '8', 0),(9, '9', 1),(10, '10', 0),(11, '11', 0),(12, '12', 0),(13, '13', 0),(14, '14', 0),(15, '15', 0),(16, '16', 0),(17, '5', 0),(18, '18', 0),(19, '19', 0),(20, '20', 0),(21, '21', 1),(22, '22', 0),(23, '23', 0),(24, '24', 0);"
 
     $sql = "INSERT IGNORE INTO rental(car_type, price) VALUES ('SUV', 200.99);";
     mysqli_query($conn, $sql);          
